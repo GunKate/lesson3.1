51cb89bfc8b5a5f61af0b3c85f0a07076eb4b210
GunKate <gokhfeld.ev@gmail.com>
2024-04-06 14:09:43 +0500

    add target1

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img/иконка.jpg")
+icon = pygame.image.load("img/Иконка.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.jpg")
+target_img = pygame.image.load("img/target1.png")
 target_width = 80
 target_height = 80
 
@@ -21,6 +21,9 @@ target_y = random.randint(0, SCREEN_HEIGHT- target_height)
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 running = True
+
+clic_count = 0
+
 while running:
     screen.fill(color)
     for event in pygame.event.get():
@@ -31,6 +34,8 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+            clic_count += 1
+            print(f'Число попаданий по цели: {clic_count}')
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
 
